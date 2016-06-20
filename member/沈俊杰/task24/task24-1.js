e94af9aa84446c22adad24a3bcd44567059c7dfb
aleko <alekoshen712@gmail.com>
2016-06-20 18:05:36 +0800

    格式化代码,task27

--- member/沈俊杰/task24/task24-1.js
+++ member/沈俊杰/task24/task24-1.js
@@ -1,12 +1,12 @@
-function ajax(opts){
+function ajax(opts) {
 	var xhr = new XMLHttpRequest();
 
-	xhr.onreadystatechange = function(){
-		if(xhr.readyState === 4 ){
-			if( ( xhr.status >= 200 && xhr.status < 300 ) || xhr.status === 304 ){
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
 				var response = JSON.parse(xhr.responseText);
 				opts.success(response);
-			}else{
+			} else {
 				opts.error();
 			}
 		}
@@ -15,17 +15,17 @@ function ajax(opts){
 	for (var key in opts.data) {
 		data += key + "=" + opts[key] + "&";
 	}
-	data = data.substr(0,data.length - 1);
-	
+	data = data.substr(0, data.length - 1);
+
 	var type = opts.type.toLowerCase(),
 		url = opts.url;
 
-	if( type  === "get" ){
+	if (type === "get") {
 		xhr.open(type, url + "?" + data, true);
 		xhr.send();
-	}else if( type === "post" ){
+	} else if (type === "post") {
 		xhr.open(type, url, true);
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhr.send(data);
+		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhr.send(data);
 	}
 }
