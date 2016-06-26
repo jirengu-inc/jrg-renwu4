c1c1036de97577f9f15c34aafb0e0428cad285d5
diulezhijue <371022026@qq.com>
2016-06-26 13:54:05 +0800

    bug

--- member/王欢/task24/code1.php
+++ member/王欢/task24/code1.php
@@ -2,8 +2,8 @@
 	$count = $_GET['count'];
 	$begin = $_GET['begin'];
 
-	$data = array('username' = > 'xiaoming','password' => 'abcd1234');
+	$data = array('username' => 'xiaoming','password' => 'abcd1234');
 
-	print_r($data)
-	echo json_encode($data)
+	// print_r($data);
+	echo json_encode($data);
 ?>
