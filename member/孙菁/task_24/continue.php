df663c6610ea480a47c16928431e1f0d4dc7c244
Rice-F <2604211051@qq.com>
2016-07-03 00:52:19 +0800

    'task-24'

--- member/孙菁/task_24/continue.php
+++ member/孙菁/task_24/continue.php
@@ -4,7 +4,7 @@ $num  = $_GET['num'];
 $sum = $start + $num ;
 $li = '';
 for($i = $start;$i< $sum;$i++){
-    $li .= "<li>内容".$i."</li>";
+    $li .= "<li>内容$i</li>";
 	}
 echo json_encode(['msg'=>$li])
 ?>
